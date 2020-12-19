69d978efcf0801d5f48499e55e4954fe13d4137b
rekcoob <marian.ivanovic88@gmail.com>
2020-12-19 17:31:03 +0100

    Serving products from backend & routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import products from './data/products';
 
 const app = express();
 
 app.get('/', (_, res) => {
 	res.send('API is running...');
+});
+app.get('/api/products', (_, res) => {
+	res.json(products);
+});
+app.get('/api/products/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
 });
 
 app.listen(5000, () => {
