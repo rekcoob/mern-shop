201956dcb8d52a6f0767ae81aed00dc8e69a1152
rekcoob <marian.ivanovic88@gmail.com>
2020-12-20 14:13:56 +0100

    Mongoose Data Models

---
+++ backend/models/userModel.ts
@@ -0,0 +1,30 @@
+import { model, Schema, Document } from 'mongoose';
+import { IUser } from '../types';
+
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default model<Document<IUser>>('User', userSchema);
---
+++ backend/types/index.ts
@@ -0,0 +1,53 @@
+export interface IUser {
+	name: string;
+	email: string;
+	password: string;
+	isAdmin: boolean;
+}
+
+export interface IProduct {
+	user: IUser;
+	name: string;
+	image: string;
+	brand: string;
+	category: string;
+	description: string;
+	reviews: { name: string; rating: number; comment: string };
+	rating: number;
+	numReviews: number;
+	price: number;
+	countInStock: number;
+}
+
+export interface IOrder {
+	user: IUser;
+	orderItems: [
+		{
+			name: string;
+			qty: number;
+			image: string;
+			price: number;
+			product: IProduct;
+		}
+	];
+	shippingAddress: {
+		address: string;
+		city: string;
+		postalCode: string;
+		country: string;
+	};
+	paymentMethod: string;
+	paymentResult: {
+		id: string;
+		status: string;
+		update_time: string;
+		email_address: string;
+	};
+	taxPrice: number;
+	shippingPrice: number;
+	totalPrice: number;
+	isPaid: boolean;
+	paidAt: any;
+	isDelivered: boolean;
+	deliveredAt: any;
+}
