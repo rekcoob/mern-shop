4c2733b5d323468ef4ffcff9792b410dedb8b6f4
rekcoob <marian.ivanovic88@gmail.com>
2020-12-22 18:38:30 +0100

    Refactor Product Route Into Controller

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -1,42 +1,11 @@
 import { Router } from 'express';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel';
+import { getProducts, getProductById } from '../controllers/productController';
 
 const router = Router();
 
-/**
- * @desc    Fetch all products
- * @route   GET /api/products
- * @access  Public
- */
-router.get(
-	'/',
-	asyncHandler(async (_, res) => {
-		const products = await Product.find({});
-		// throw new Error('Some error');
+// router.get('/', getProducts);
+router.route('/').get(getProducts);
 
-		res.json(products);
-	})
-);
-
-/**
- * @desc    Fetch single product
- * @route   GET /api/products/:id
- * @access  Public
- */
-router.get(
-	'/:id',
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-		console.log(product);
-
-		if (product) {
-			res.json(product);
-		} else {
-			res.status(404);
-			throw new Error('Product not found');
-		}
-	})
-);
+router.route('/:id').get(getProductById);
 
 export default router;
