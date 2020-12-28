026f368993ab1d2102063991c9a3d9270cab1789
rekcoob <marian.ivanovic88@gmail.com>
2020-12-28 14:52:02 +0100

    Checkout Steps Component

--- frontend/src/pages/ShippingPage.tsx
+++ frontend/src/pages/ShippingPage.tsx
@@ -3,6 +3,8 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { FormContainer } from '../components/FormContainer';
+import { CheckoutSteps } from '../components/CheckoutSteps';
+
 import { saveShippingAddress } from '../store/actions/cartActions';
 import { RootState } from '../store/types/rootTypes';
 
@@ -28,6 +30,7 @@ export const ShippingPage: React.FC = () => {
 
 	return (
 		<FormContainer>
+			<CheckoutSteps step1 step2 />
 			<h1>Shipping</h1>
 			<Form onSubmit={handleSubmit}>
 				<Form.Group controlId="address">
