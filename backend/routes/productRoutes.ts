6bf65d395c91a41b63ed26d851147baa6f1157c9
rekcoob <marian.ivanovic88@gmail.com>
2021-01-03 13:52:26 +0100

    Create and update product endpoints

--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -3,14 +3,20 @@ import {
 	getProducts,
 	getProductById,
 	deleteProduct,
+	updateProduct,
+	createProduct,
 } from '../controllers/productController';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = Router();
 
 // router.get('/', getProducts);
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router
+	.route('/:id')
+	.get(getProductById)
+	.delete(protect, admin, deleteProduct)
+	.put(protect, admin, updateProduct);
 
 export default router;
